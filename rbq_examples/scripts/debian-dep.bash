3730f5734b348f766ca9f274de8fec4203609823
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2026-05-21 21:55:07 +0800

    initial working image

--- rbq_examples/scripts/debian-dep.bash
+++ rbq_examples/scripts/debian-dep.bash
@@ -15,6 +15,28 @@ DEBIAN_FRONTEND=noninteractive apt -y --quiet --no-install-recommends install  \
     git curl wget sshpass libssl-dev libcurl4-openssl-dev \
     python3 python3-dev python3-pip \
     libeigen3-dev \
+    libasio-dev \
+    locales \
+    mesa-common-dev \
+    libglu1-mesa-dev libglfw3-dev libglew-dev libgl1-mesa-dev libsoup2.4-dev libglib2.0-dev \
+    libpng-dev libflatbuffers-dev libtins-dev libpcap-dev libsuitesparse-dev libgtk-3-dev \
+    '^libxcb.*-dev' libx11-xcb-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb1-dev libx11-dev libxext-dev libxcb-* \
+    libxrandr-dev libxinerama-dev libxcursor-dev libxfixes-dev \
+    ca-certificates \
+    libsdl2-dev \
+    libspdlog-dev \
+    apt-transport-https \
+    fuse3 \
+    v4l-utils libusb-1.0-0-dev \
+    lsb-release \
+    meson flex bison \
+    yasm \
+    doxygen \
+    libopencv-dev \
+    libopencv-contrib-dev \
+    ffmpeg \
+    libwebp-dev \
+    tmux
 
 echo "✅ apt installed."
 
